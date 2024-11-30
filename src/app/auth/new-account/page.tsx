b04ebe5e284ac71dbeb3fbce10c317f0a29a9dc3
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-11-30 04:36:58 -0600

    Create basic layout for login and signup pages

--- src/app/auth/new-account/page.tsx
+++ src/app/auth/new-account/page.tsx
@@ -1,7 +1,34 @@
-export default function LoginPage() {
+import { titleFont } from '@/config/fonts';
+import Link from 'next/link';
+
+export default function NewAccountPage() {
   return (
-    <div>
-      <h1>New account page</h1>
-    </div>
+    <main className="flex flex-col min-h-screen pt-32 sm:pt-52">
+      <h1 className={`${titleFont.className} text-4xl mb-5`}>New account</h1>
+
+      <div className="flex flex-col">
+        <label htmlFor="email">Full name</label>
+        <input className="px-5 py-2 border bg-gray-200 rounded mb-5" type="text" />
+
+        <label htmlFor="email">Email</label>
+        <input className="px-5 py-2 border bg-gray-200 rounded mb-5" type="email" />
+
+        <label htmlFor="email">Password</label>
+        <input className="px-5 py-2 border bg-gray-200 rounded mb-5" type="password" />
+
+        <button className="btn-primary">Log in</button>
+
+        {/* dividing line */}
+        <div className="flex items-center my-5">
+          <div className="flex-1 border-t border-gray-500"></div>
+          <div className="px-2 text-gray-800">OR</div>
+          <div className="flex-1 border-t border-gray-500"></div>
+        </div>
+
+        <Link href="/auth/login" className="btn-secondary text-center">
+          Already have an account?
+        </Link>
+      </div>
+    </main>
   );
 }
