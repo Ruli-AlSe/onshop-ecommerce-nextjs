1e9707e00d8e0c00616f79e1a09c79a00bdaac26
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-11-30 02:39:01 -0600

    Implement product details page

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,7 +1,64 @@
-export default function ProductPage() {
+import { notFound } from 'next/navigation';
+
+import {
+  ProductMobileSlideshow,
+  ProductSlideshow,
+  QuantitySelector,
+  SizeSelector,
+} from '@/components';
+import { titleFont } from '@/config/fonts';
+import { initialData } from '@/seed/seed';
+
+interface Props {
+  params: Promise<{
+    slug: string;
+  }>;
+}
+
+export default async function ProductPage({ params }: Props) {
+  const { slug } = await params;
+  const product = initialData.products.find((product) => product.slug === slug);
+
+  if (!product) {
+    notFound();
+  }
+
   return (
-    <div>
-      <h1>Product Page</h1>
+    <div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
+      {/* slideshow */}
+      <div className="col-span-1 md:col-span-2">
+        {/* mobile slideshow */}
+        <ProductMobileSlideshow
+          title={product.title}
+          images={product.images}
+          className="block md:hidden"
+        />
+
+        {/* desktop slideshow */}
+        <ProductSlideshow
+          title={product.title}
+          images={product.images}
+          className="hidden md:block"
+        />
+      </div>
+
+      {/* details */}
+      <div className="col-span-1 px-5">
+        <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>{product.title}</h1>
+        <p className="text-lg mb-5">${product.price.toFixed(2)}</p>
+
+        {/* sizes selector */}
+        <SizeSelector sizeSelected={product.sizes[0]} availableSizes={product.sizes} />
+
+        {/* quantity selector */}
+        <QuantitySelector quantity={2} />
+
+        <button className="btn-primary my-5">Add to cart</button>
+
+        {/* description */}
+        <h3 className="font-bold text-sm">Description</h3>
+        <p className="font-light">{product.description}</p>
+      </div>
     </div>
   );
 }
