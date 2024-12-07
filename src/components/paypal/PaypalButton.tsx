a216abbd3354550d9a66f1e6aca083a8f484fe28
Raul Almanza <ra.almanzaserrano@gmail.com>
2024-12-07 05:05:50 -0600

    Implement payment and payment verification with paypal

---
+++ src/components/paypal/PaypalButton.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import { paypalCheckPayment, setTransactionId } from '@/actions';
+import {
+  CreateOrderActions,
+  CreateOrderData,
+  OnApproveActions,
+  OnApproveData,
+} from '@paypal/paypal-js';
+import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js';
+
+interface Props {
+  orderId: string;
+  amount: number;
+}
+
+export const PaypalButton = ({ orderId, amount }: Props) => {
+  const [{ isPending }] = usePayPalScriptReducer();
+  const roundedAmount = Math.round(amount * 100) / 100;
+
+  if (isPending) {
+    return (
+      <div className="animate-pulse mb-7">
+        <div className="h-11 bg-gray-300 rounded" />
+        <div className="h-11 bg-gray-300 rounded mt-2" />
+      </div>
+    );
+  }
+
+  const createOrder = async (
+    data: CreateOrderData,
+    actions: CreateOrderActions
+  ): Promise<string> => {
+    const transactionId = await actions.order.create({
+      purchase_units: [
+        {
+          invoice_id: orderId,
+          amount: {
+            currency_code: 'USD',
+            value: roundedAmount.toString(),
+          },
+        },
+      ],
+      intent: 'CAPTURE',
+    });
+
+    const { ok } = await setTransactionId(orderId, transactionId);
+    if (!ok) {
+      throw new Error('Order was not updated');
+    }
+
+    return transactionId;
+  };
+
+  const onApprove = async (data: OnApproveData, actions: OnApproveActions) => {
+    const details = await actions.order?.capture();
+    if (!details) return;
+
+    await paypalCheckPayment(details.id ?? '');
+  };
+
+  return <PayPalButtons createOrder={createOrder} onApprove={onApprove} />;
+};
